ef30b4bb729534c82a39c74d891a9ad29473ac98
Hyperlotl <hyperlotl@gmail.com>
2025-11-17 22:11:10 +0800

    code cleanup + removed HTML version

--- image_logic.gd
+++ image_logic.gd
@@ -1,7 +1,10 @@
-extends Sprite2D
-@onready var sprite_colour_ramp = texture.color_ramp
+extends Node2D
 @export var seed = 0
-var resolution = 16
+@onready var display = $"generatedPatternDisplay"
+@onready var generated_noise = $generatedPattern
+@onready var sprite_colour_ramp = generated_noise.texture.color_ramp
+
+var resolution = 64
 var colours = [
 	Color(0,0,1,1),
 	Color(0,1,1,1),
@@ -19,29 +22,24 @@ func _ready() -> void:
 		offsets.append(float(i)/6)
 	generated_gradient.offsets = PackedFloat32Array(offsets)
 	generated_gradient.colors =	PackedColorArray(colours)	
-	texture.color_ramp = generated_gradient
+	generated_noise.texture.color_ramp = generated_gradient
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	resolution = $"../PatternResolutionInput/HSlider".value
+	resolution = $PatternResolutionInput/HSlider.value
 	resolution = 2 ** resolution
-	$"../PatternResolutionInput/Label2".text = "Current Resolution: " + str(resolution) + " px"
+	$"PatternResolutionInput/Label2".text = "Current Resolution: " + str(resolution) + " px"
 	var scale_mult = 16*32/resolution
-	visible = false
-	scale = Vector2(scale_mult,scale_mult)
-	texture.noise.seed = int($"../SeedInput/TextEdit".text)
-	texture.noise.frequency = (16.0/(100*resolution))
-	texture.width = resolution
-	texture.height = resolution
-	visible = true
-
-	pass
+	generated_noise.texture.noise.seed = int($"SeedInput/TextEdit".text)
+	generated_noise.texture.noise.frequency = (16.0/(100*resolution))
+	generated_noise.texture.width = resolution
+	generated_noise.texture.height = resolution
+	display.scale = Vector2(scale_mult,scale_mult)
+	display.texture = ImageTexture.create_from_image(generated_noise.texture.get_image())
+	print("If the code is running correctly, this should display 0: "+str(display.texture.get_width()-resolution))
 
 
 func _on_button_pressed() -> void:
-	var tex := texture
-	var img := tex.get_image()
+	var img = generated_noise.texture.get_image()
 	var downloads_path = OS.get_system_dir(OS.SYSTEM_DIR_DOWNLOADS)
-	print(downloads_path)
-# Save the image there
 	img.save_png(downloads_path + "/export.png")
